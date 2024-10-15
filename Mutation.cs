9bd9cd014aedfb2d33e7ab84516a8ae07e57da45
Erik O'Leary <erik.m.oleary@gmail.com>
2024-10-15 16:11:25 -0500

    Add mutation

--- Mutation.cs
+++ Mutation.cs
@@ -1,25 +1,25 @@
 namespace Repro.GraphQLMutations;
 
-// [MutationType]
-// public class Mutation
-// {
-//     /// <summary>
-//     /// Add a thingy
-//     /// </summary>
-//     public bool AddThingy(CouldBeInputThingInput thing)
-//     {
-//         return true;
-//     }
-// }
+[MutationType]
+public class Mutation
+{
+    /// <summary>
+    /// Add a thingy
+    /// </summary>
+    public bool AddThingy(CouldBeInputThingInput thing)
+    {
+        return true;
+    }
+}
 
-// [OneOf]
-// public class CouldBeInputThingInput
-// {
-//     public Boat? Boat { get; set; }
+[OneOf]
+public class CouldBeInputThingInput
+{
+    public Boat? Boat { get; set; }
 
-//     public Car? Car { get; set; }
-// }
+    public Car? Car { get; set; }
+}
 
-// public class BoatInput : InputObjectType<Boat>;
+public class BoatInput : InputObjectType<Boat>;
 
-// public class CarInput : InputObjectType<Car>;
+public class CarInput : InputObjectType<Car>;
